c07bba14b620e3e48e6afd7a6003bbe09a360f0e
Manuel Ignacio Luis Fuentes <mluis-fu@c1r12s3.42madrid.com>
2022-07-10 15:52:40 +0200

    trying out

--- get_next_line.c
+++ get_next_line.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mluis-fu <mluis-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 12:49:38 by manuel            #+#    #+#             */
-/*   Updated: 2022/07/10 15:24:31 by manuel           ###   ########.fr       */
+/*   Updated: 2022/07/10 15:52:19 by mluis-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* this function checks if there is a line to copy, and stores it in a 
-dynamic array and returns the new line with a line feed and a null.*/
+/*
+this function checks if there is a line to copy, and stores it in a 
+dynamic array and returns the new line with a line feed and a null.
+*/
 
-char    *check_line(char *str)
+char	*check_line(char *str)
 {
-	int     count;
-	char    *line;
-	
+	int		count;
+	char	*line;
+
 	count = 0;
-	while(str[count] && str[count] != '\n')
+	while (str[count] && str[count] != '\n')
 		count++;
 	line = ft_calloc(count + 2, 1);
 	count = 0;
@@ -30,11 +32,13 @@ char    *check_line(char *str)
 	return (line);
 }
 
-/*this function, is to store the remaining content of the file 
+/*
+this function, is to store the remaining content of the file 
 (remaining content == to content after the line to be displayed)
 we create a dynamic array with the size of the BUFFER_SIZE + 1, 
-and there we store what we want to be copied from the line, we join to save that is
-the function passed by parameter to be returned, and line is released just at the end of the function
+and there we store what we want to be copied from the line, we join to save 
+that is the function passed by parameter to be returned, and line is 
+released just at the end of the function
 */
 char	*read_and_save(int fd, char *buff)
 {
@@ -48,7 +52,7 @@ char	*read_and_save(int fd, char *buff)
 	while (!ft_strchr(buff, '\n') && bytes_count != 0)
 	{
 		bytes_count = read(fd, sub_buff, BUFFER_SIZE);
-		if (bytes_count = -1)
+		if (bytes_count == -1)
 		{
 			free (sub_buff);
 			return (NULL);
@@ -59,6 +63,12 @@ char	*read_and_save(int fd, char *buff)
 	return (buff);
 }
 
+/*
+this function is used to store the rest of the file, if it exists, so it 
+assigns a ft_strlen - i, where i is the first line to copy, and the rest 
+I want to store for my static variable to continue reading in case the 
+function is called with a loop.
+*/
 char	*rest_of_file(char *buffer)
 {
 	int		i;
@@ -73,12 +83,22 @@ char	*rest_of_file(char *buffer)
 		return (NULL);
 	i++;
 	j = 0;
-	while(buffer[i])
+	while (buffer[i])
 		sub_buffer[j++] = buffer [i++];
 	free (buffer);
 	return (sub_buffer);
 }
 
+/*
+the function get_next_line makes first a call to read_and_save, which r
+eturns the number of characters defined by BUFFER_SIZE in a char * type 
+variable, after checking if such information exists in the variable, it is 
+passed by the function check_line, which copies the content of the previous 
+variable in a new one with dynamic memory and it is returned, so that it is our 
+final return value in the function. and finally a call is made to rest_of_file,
+this stores the rest of the file descriptor, in a static variable of type 
+char * so that the function can continue to be called in loop!
+*/
 char	*get_next_line(int fd)
 {
 	char		*line_print;
@@ -87,7 +107,7 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = read_and_save (fd, buffer);
-	if (!line_print)
+	if (!buffer)
 		return (NULL);
 	line_print = check_line(buffer);
 	buffer = rest_of_file(buffer);
